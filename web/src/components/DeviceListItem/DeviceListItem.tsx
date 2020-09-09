7035e7b63535ddac0f4d042877d3b3fbe0bf3fed
kingsleyokyere <kkbokyere@gmail.com>
2020-09-09 16:43:34 +0100

    - update queiries to return plant

--- web/src/components/DeviceListItem/DeviceListItem.tsx
+++ web/src/components/DeviceListItem/DeviceListItem.tsx
@@ -3,7 +3,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import ImageIcon from '@material-ui/icons/Image';
+import DeviceHub from '@material-ui/icons/DeviceHub';
 import Divider from '@material-ui/core/Divider';
 
 import {Plant} from "../../types";
@@ -21,10 +21,10 @@ const DeviceListItem = ({ id, label, plant }: Props) => {
         <ListItem>
             <ListItemAvatar>
                 <Avatar>
-                    <ImageIcon />
+                    <DeviceHub />
                 </Avatar>
             </ListItemAvatar>
-            <ListItemText primary={label} secondary={plant.name}/>
+            <ListItemText primary={label} secondary={`${plant.name}`}/>
             <Divider/>
         </ListItem>)
 };
